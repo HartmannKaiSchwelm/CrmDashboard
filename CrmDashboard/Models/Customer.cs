484236b99231aa6fa19f24c6bcbe4aaf934b6638
Kai <Pappa@DESKTOP-29NAR0C>
2025-05-10 09:56:17 +0200

    added Customer Model und mockup data in Index.razor

---
+++ CrmDashboard/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace CrmDashboard.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public string Company { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public decimal Revenue { get; set; }
+
+    }
+}
